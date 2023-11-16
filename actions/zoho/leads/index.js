326d239905da7ed881c9a118d3a8686d55d3d9f5
Kevin Andrews <kevin@bonanza.com>
2023-11-15 18:27:09 -0800

    fix lookupLead function

--- actions/zoho/leads/index.js
+++ actions/zoho/leads/index.js
@@ -1,23 +1,27 @@
-'use server'
-import axios from "axios";
-import { getZohoAccount } from "..";
+'use server';
+import axios from 'axios';
+import { getZohoAccount } from '..';
 
 export const lookupLead = async ({ from, studioId }) => {
-    const account = await getZohoAccount({ studioId });
+  const account = await getZohoAccount({ studioId });
 
-    try {
-        const response = await axios.get(`https://www.zohoapis.com/crm/v5/Leads/search?criteria=(Mobile:equals:${from})`, {
-            headers: { Authorization: `Zoho-oauthtoken ${account.accessToken}` },
-        });
-        const data = response.data;
-
-        if (data.length === 0) {
-            return { leadId: null, leadName: null };
+  try {
+    const { data } = await axios
+      .get(
+        `https://www.zohoapis.com/crm/v5/Leads/search?criteria=(Mobile:equals:${from})`,
+        {
+          headers: { Authorization: `Zoho-oauthtoken ${account.accessToken}` },
         }
+      )
+      .then((res) => res.data);
 
-        return { leadId: data[0].id, leadName: data[0].Full_Name };
-    } catch (error) {
-        console.error('Error looking up lead:', error);
-        return { leadId: null, leadName: null };
+    if (data.length === 0) {
+      return { leadId: null, leadName: null };
     }
+
+    return { leadId: data[0].id, leadName: data[0].Full_Name };
+  } catch (error) {
+    console.error('Error looking up lead:', error);
+    return { leadId: null, leadName: null };
+  }
 };
