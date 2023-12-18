2abf3828eb48b216b4a8d1d9bedc7da1f641d667
Kevin Andrews <kevin@bonanza.com>
2023-12-17 20:18:30 -0800

    typo

--- actions/twilio/index.js
+++ actions/twilio/index.js
@@ -22,89 +22,90 @@ export const getTwilioAccount = async (id) => {
   } catch (error) {
     console.error('Error getting Twilio account:', error);
     throw error;
-  };
+  }
+}
 
-  const getTwilioClient = ({ clientId, clientSecret }) => twilio(clientId, clientSecret);
+const getTwilioClient = ({ clientId, clientSecret }) => twilio(clientId, clientSecret);
 
-  export const getMessagesToContact = async (client, leadPhoneNumber) => {
-    try {
-      const response = await client.messages.list({ to: leadPhoneNumber });
-      return response.map((message) => ({
-        to: message.to,
-        from: message.from,
-        body: message.body,
-        date: message.dateSent,
-        fromStudio: true,
-      }));
-    } catch (error) {
-      console.error('Error getting messages to contact:', error);
-      throw error;
-    }
+export const getMessagesToContact = async (client, leadPhoneNumber) => {
+  try {
+    const response = await client.messages.list({ to: leadPhoneNumber });
+    return response.map((message) => ({
+      to: message.to,
+      from: message.from,
+      body: message.body,
+      date: message.dateSent,
+      fromStudio: true,
+    }));
+  } catch (error) {
+    console.error('Error getting messages to contact:', error);
+    throw error;
+  }
 
-  };
+};
 
-  export const getMessagesFromContact = async (client, leadPhoneNumber) => {
-    try {
-      const response = await client.messages.list({ from: leadPhoneNumber });
-      return response.map((message) => ({
-        to: message.to,
-        from: message.from,
-        body: message.body,
-        date: message.dateSent,
-        fromStudio: false,
-      }));
-    } catch (error) {
-      console.error('Error getting messages from contact:', error);
-      throw error;
-    }
+export const getMessagesFromContact = async (client, leadPhoneNumber) => {
+  try {
+    const response = await client.messages.list({ from: leadPhoneNumber });
+    return response.map((message) => ({
+      to: message.to,
+      from: message.from,
+      body: message.body,
+      date: message.dateSent,
+      fromStudio: false,
+    }));
+  } catch (error) {
+    console.error('Error getting messages from contact:', error);
+    throw error;
+  }
 
-  };
+};
 
-  export const getMessages = async ({ leadPhoneNumber, studioId }) => {
-    try {
-      const twilioAccount = await getTwilioAccount(studioId);
-
-      if (!twilioAccount) {
-        throw new Error('Could not find Twilio account');
-      }
-
-      const client = getTwilioClient(twilioAccount);
-
-      const messagesToContact = await getMessagesToContact(client, leadPhoneNumber);
-      const messagesFromContact = await getMessagesFromContact(client, leadPhoneNumber);
-
-      return [...messagesToContact, ...messagesFromContact].sort(
-        (a, b) => new Date(a.date) - new Date(b.date)
-      );
-    } catch (error) {
-      console.error('Error getting messages:', { leadPhoneNumber, studioId });
-      throw error;
-    }
-
-  };
-
-
-  // Create a route to send a new text message
-  export const sendMessage = async ({ to, from, message, studioId }) => {
+export const getMessages = async ({ leadPhoneNumber, studioId }) => {
+  try {
     const twilioAccount = await getTwilioAccount(studioId);
 
     if (!twilioAccount) {
-      console.error('Could not find Twilio account');
-      return;
+      throw new Error('Could not find Twilio account');
     }
 
     const client = getTwilioClient(twilioAccount);
 
-    try {
-      const sendRecord = await client.messages.create({
-        body: message,
-        from,
-        to,
-      });
-      console.log(sendRecord)
-      return sendRecord.sid
-    } catch (error) {
-      console.error('Error sending message:', { to, from, message, studioId })
-      throw error;
-    }
-  };
+    const messagesToContact = await getMessagesToContact(client, leadPhoneNumber);
+    const messagesFromContact = await getMessagesFromContact(client, leadPhoneNumber);
+
+    return [...messagesToContact, ...messagesFromContact].sort(
+      (a, b) => new Date(a.date) - new Date(b.date)
+    );
+  } catch (error) {
+    console.error('Error getting messages:', { leadPhoneNumber, studioId });
+    throw error;
+  }
+
+};
+
+
+// Create a route to send a new text message
+export const sendMessage = async ({ to, from, message, studioId }) => {
+  const twilioAccount = await getTwilioAccount(studioId);
+
+  if (!twilioAccount) {
+    console.error('Could not find Twilio account');
+    return;
+  }
+
+  const client = getTwilioClient(twilioAccount);
+
+  try {
+    const sendRecord = await client.messages.create({
+      body: message,
+      from,
+      to,
+    });
+    console.log(sendRecord)
+    return sendRecord.sid
+  } catch (error) {
+    console.error('Error sending message:', { to, from, message, studioId })
+    throw error;
+  }
+};
