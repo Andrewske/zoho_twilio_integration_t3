67a6b9ef62ea286974da6d11d1329f78d20c07eb
Kevin Andrews <kevin@bonanza.com>
2023-12-19 17:07:35 -0800

    remove smooth scrolling

--- components/MessageList/index.jsx
+++ components/MessageList/index.jsx
@@ -7,25 +7,11 @@ const MessageList = ({ messages }) => {
   const messagesEndRef = useRef(null);
   const [prevMessageLength, setPrevMessageLength] = useState(0);
 
-  const smoothScrollToBottom = (element) => {
-    const duration = 400; // duration in ms
-    const distance = element.scrollHeight - element.scrollTop;
-    const perTick = (distance / duration) * 10;
-
-    const scrollInterval = setInterval(() => {
-      if (element.scrollTop === element.scrollHeight) {
-        clearInterval(scrollInterval);
-      } else {
-        element.scrollTop += perTick;
-      }
-    }, 10);
-  };
-
   useEffect(() => {
     if (messages.length > prevMessageLength) {
       setPrevMessageLength(messages.length);
       const scrollableDiv = messagesEndRef.current.parentElement;
-      smoothScrollToBottom(scrollableDiv);
+      scrollableDiv.scrollTop = scrollableDiv.scrollHeight;
     }
   }, [messages.length, prevMessageLength]);
 
