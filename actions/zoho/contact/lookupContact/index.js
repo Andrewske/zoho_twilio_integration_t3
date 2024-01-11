23c9d495e71cb824cef9c238d6832c3f74ae9556
Kevin Andrews <kevin@bonanza.com>
2024-01-10 18:12:37 -0800

    remove retry operation and add update sentFollowUpMessage

--- actions/zoho/contact/lookupContact/index.js
+++ actions/zoho/contact/lookupContact/index.js
@@ -5,108 +5,109 @@ import { getZohoAccount } from '~/actions/zoho';
 import { logError } from '~/utils/logError';
 
 const formatMobile = (mobile) => {
-    return mobile.replace(/\D/g, '');
+  return mobile.replace(/\D/g, '');
 };
 
 const axiosGetContact = async ({ mobile, account, zohoModule }) => {
-    const fields = 'id,Full_Name,Mobile,SMS_Opt_Out,Lead_Status,Owner';
-    const criteria = `(Mobile:equals:${formatMobile(mobile)})`;
-    const response = await axios.get(
-        `https://www.zohoapis.com/crm/v5/${zohoModule}/search?fields=${fields}&criteria=${criteria}`,
-        {
-            headers: { Authorization: `Zoho-oauthtoken ${account.accessToken}` },
-        }
-    );
-
-    if (response.status !== 200) {
-        throw new Error(`Request failed with status code ${response.status}`);
+  const fields = 'id,Full_Name,Mobile,SMS_Opt_Out,Lead_Status,Owner';
+  const criteria = `(Mobile:equals:${formatMobile(mobile)})`;
+  const response = await axios.get(
+    `https://www.zohoapis.com/crm/v5/${zohoModule}/search?fields=${fields}&criteria=${criteria}`,
+    {
+      headers: { Authorization: `Zoho-oauthtoken ${account.accessToken}` },
     }
+  );
 
-    const data = response?.data?.data;
-    if (!data || !data[0]) {
-        throw new Error('No data returned from server');
-    }
+  if (response.status !== 200) {
+    throw new Error(`Request failed with status code ${response.status}`);
+  }
 
-    const contact = {
-        ...data[0],
-        isLead: zohoModule === 'Leads',
-    }
-    return contact;
+  const data = response?.data?.data;
+  if (!data || !data[0]) {
+    throw new Error('No data returned from server');
+  }
+
+  const contact = {
+    ...data[0],
+    isLead: zohoModule === 'Leads',
+  };
+  return contact;
 };
 
 const getContact = async ({ mobile, account, zohoModule }) => {
-    try {
-        return await axiosGetContact({ mobile, account, zohoModule });
-    } catch (error) {
-        return null;
-    }
+  try {
+    return await axiosGetContact({ mobile, account, zohoModule });
+  } catch (error) {
+    return null;
+  }
 };
 
 const getContactFromModules = async ({ mobile, account, modules }) => {
-    for (const zohoModule of modules) {
-        try {
-            const contact = await getContact({ mobile, account, zohoModule });
-            if (contact) {
-                return contact;
-            }
-        } catch (error) {
-            console.error(`Error looking up contact in ${module}:`);
-        }
-    }
-    return null;
-};
-
-export const lookupContact = async ({ mobile, studioId, retry = false }) => {
-    if (!mobile) {
-        throw new Error('No number provided');
-    }
-
-    const account = await getZohoAccount({ studioId });
-
-    if (!account) {
-        throw new Error('Could not find Zoho account');
-    }
-
-    const zohoModules = ['Leads', 'Contacts'];
-    const operation = () => getContactFromModules({ mobile, account, modules: zohoModules });
-
-    if (retry) {
-        const contact = await retryOperation(operation, 10000, 5);
-        if (!contact) {
-            throw new Error('No contact found after 5 retries');
-        }
+  for (const zohoModule of modules) {
+    try {
+      const contact = await getContact({ mobile, account, zohoModule });
+      if (contact) {
         return contact;
+      }
+    } catch (error) {
+      console.error(`Error looking up contact in ${module}:`);
     }
-
-    const contact = await operation();
-    if (!contact) {
-        logError({ error: new Error('Not contact after 5 retries'), message: 'No contact found', data: { mobile, studioId }, level: 'warning' })
-        throw new Error('No contact found');
-    }
-    return contact;
+  }
+  return null;
 };
 
-const retryOperation = async (operation, delay, maxRetries) => {
-    console.log('retryOperation')
-    let attempts = 0;
-    while (attempts < maxRetries) {
-        try {
-            const result = await operation();
-            if (result) {
-                console.log(`Result found after ${attempts} attempts`);
-                return result;
-            }
-            // If result is not found, throw an error to go to the catch block.
-            throw new Error('Result not found');
-        } catch (error) {
-            console.log(`Error in retryOperation, attempt: ${attempts}`)
-            if (attempts === maxRetries - 1) {
-                throw error;
-            }
-            console.log(`Retrying in ${delay * Math.pow(2, attempts)} ms`);
-            await new Promise(resolve => setTimeout(resolve, delay * Math.pow(2, attempts)));
-            attempts++;
-        }
-    }
-    return null; // In case of max retries reached
+export const lookupContact = async ({ mobile, studioId }) => {
+  if (!mobile) {
+    throw new Error('No number provided');
+  }
+
+  const account = await getZohoAccount({ studioId });
+
+  if (!account) {
+    throw new Error('Could not find Zoho account');
+  }
+
+  const zohoModules = ['Leads', 'Contacts'];
+
+  const contact = await getContactFromModules({
+    mobile,
+    account,
+    modules: zohoModules,
+  });
+
+  if (!contact) {
+    logError({
+      error: new Error('Not contact found'),
+      message: 'No contact found',
+      data: { mobile, studioId },
+      level: 'warning',
+    });
+    throw new Error('No contact found');
+  }
+  return contact;
 };
+
+// const retryOperation = async (operation, delay, maxRetries) => {
+//     console.log('retryOperation')
+//     let attempts = 0;
+//     while (attempts < maxRetries) {
+//         try {
+//             const result = await operation();
+//             if (result) {
+//                 console.log(`Result found after ${attempts} attempts`);
+//                 return result;
+//             }
+//             // If result is not found, throw an error to go to the catch block.
+//             throw new Error('Result not found');
+//         } catch (error) {
+//             console.log(`Error in retryOperation, attempt: ${attempts}`)
+//             if (attempts === maxRetries - 1) {
+//                 throw error;
+//             }
+//             console.log(`Retrying in ${delay * Math.pow(2, attempts)} ms`);
+//             await new Promise(resolve => setTimeout(resolve, delay * Math.pow(2, attempts)));
+//             attempts++;
+//         }
+//     }
+//     return null; // In case of max retries reached
+// };
--- app/api/twilio/webhook/route.js
+++ app/api/twilio/webhook/route.js
@@ -6,7 +6,7 @@ import { smsOptOut } from '~/actions/zoho/contact/smsOptOut';
 import { lookupContact } from '~/actions/zoho/contact/lookupContact';
 import { updateStatus } from '~/actions/zoho/contact/updateStatus';
 import { sendMessage } from '~/actions/twilio';
-import { logError } from "~/utils/logError"
+import { logError } from '~/utils/logError';
 
 export async function POST(request) {
   var STOP = false;
@@ -19,7 +19,7 @@ export async function POST(request) {
     }
 
     let { To: to, From: from, Body: msg } = message;
-    console.log(JSON.stringify({ to, from, msg }))
+    console.log(JSON.stringify({ to, from, msg }));
 
     if (to.startsWith('+1')) {
       to = to.substring(2);
@@ -38,16 +38,26 @@ export async function POST(request) {
       throw new Error('Could not find studio');
     }
 
-    const contact = await lookupContact({ mobile: from, studioId: studioInfo.id, retry: true });
+    const contact = await lookupContact({
+      mobile: from,
+      studioId: studioInfo.id,
+    });
 
-    if (contact.isLead && contact.Lead_Status == 'New' && msg.toLowerCase().includes('yes')) {
-      updateStatus({ studio: studioInfo, contact })
-      sendFollowUpMessage({ contact, from, to, studioInfo })
+    if (
+      contact.isLead &&
+      contact.Lead_Status == 'New' &&
+      msg.toLowerCase().includes('yes')
+    ) {
+      updateStatus({ studio: studioInfo, contact });
+      sendFollowUpMessage({ contact, from, to, studioInfo });
     }
 
     // TODO: make sure cleaned to and from values are being sent to postWebhookData
-    await postWebhookData({ message, studioId: studioInfo.id, contactId: contact.id })
-
+    await postWebhookData({
+      message,
+      studioId: studioInfo.id,
+      contactId: contact.id,
+    });
 
     if (STOP) {
       await smsOptOut({ studio: studioInfo, contact });
@@ -61,22 +71,24 @@ export async function POST(request) {
     }
   } catch (error) {
     console.error('Error in Twilio Webhook:', error);
-    throw new Error('Webhook error')
+    throw new Error('Webhook error');
   }
   return new Response(null, { status: 200 });
 }
 
 async function postWebhookData({ message, studioId, contactId }) {
-  return prisma.twilioMessage.create({
-    data: {
-      studioId,
-      contactId,
-      from: message.From,
-      to: message.To,
-      message: message.Body,
-      twilioMessageId: message.MessageSid,
-    },
-  }).then(({ id }) => id);
+  return prisma.twilioMessage
+    .create({
+      data: {
+        studioId,
+        contactId,
+        from: message.From,
+        to: message.To,
+        message: message.Body,
+        twilioMessageId: message.MessageSid,
+      },
+    })
+    .then(({ id }) => id);
 }
 
 export async function parseRequest(request) {
@@ -84,7 +96,12 @@ export async function parseRequest(request) {
     const body = await request.text();
     return parse(body);
   } catch (error) {
-    logError({ message: 'Error parsing request:', error, level: "warning", data: { request } })
+    logError({
+      message: 'Error parsing request:',
+      error,
+      level: 'warning',
+      data: { request },
+    });
   }
 }
 
@@ -93,36 +110,56 @@ export async function isValidMessage(message) {
 }
 
 export async function getStudioInfo(to) {
-
   try {
     return await prisma.studio.findFirst({
       where: { smsPhone: to.replace('+1', '') },
       select: { id: true, zohoId: true },
     });
   } catch (error) {
-    logError({ message: 'Could not find studio', error, level: "warning", data: { to } })
+    logError({
+      message: 'Could not find studio',
+      error,
+      level: 'warning',
+      data: { to },
+    });
   }
 }
 
-
 async function sendFollowUpMessage({ contact, from, to, studioInfo }) {
-  console.log('sending follow up message')
   try {
-    const welcomeMessageRecord = await prisma.zohoWebhook.findFirst({
-      where: { contactId: contact.id, sentWelcomeMessage: true }
-    })
+    const contactZohoRecord = await prisma.zohoWebhook.findFirst({
+      where: {
+        contactId: contact.id,
+      },
+    });
 
+    if (!contactZohoRecord?.sentFollowUpMessage) {
+      const followUpMessage =
+        'Great! We have a limited number spots for new clients each week. What day of the week Monday to Friday works best for you?';
+      const response = sendMessage({
+        to: from,
+        from: to,
+        message: followUpMessage,
+        studioId: studioInfo.id,
+        contact,
+      });
 
-
-    if (!welcomeMessageRecord) {
-      const followUpMessage = "Great! We have a limited number spots for new clients each week. What day of the week Monday to Friday works best for you?"
-      sendMessage({ to: from, from: to, message: followUpMessage, studioId: studioInfo.id, contact })
+      if (response?.twilioMessageId) {
+        await prisma.zohoWebhook.update({
+          where: { id: contactZohoRecord.id },
+          data: { sentFollowUpMessage: true },
+        });
+      }
     } else {
-      console.log('already sent follow up message')
+      console.log('already sent follow up message');
     }
-
   } catch (error) {
     console.error('Error sending follow up message:', error);
-    logError({ message: 'Error sending follow up message:', error, level: "warning", data: { contactId: contact?.id, from, to, studioId: studioInfo?.zohoId } })
+    logError({
+      message: 'Error sending follow up message:',
+      error,
+      level: 'warning',
+      data: { contactId: contact?.id, from, to, studioId: studioInfo?.zohoId },
+    });
   }
 }
