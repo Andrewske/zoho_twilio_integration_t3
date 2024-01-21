05f24aec55c9a4edf11496cfc761353db9edaa95
Kevin Andrews <kevin@bonanza.com>
2024-01-20 23:10:01 -0800

    logg message

--- actions/sendFollowUpMessage/index.js
+++ actions/sendFollowUpMessage/index.js
@@ -25,10 +25,10 @@ const sendFollowUpMessage = async ({ contact, from, to, studioId }) => {
 
   console.log({ twilioMessageId: response.twilioMessageId });
 
-  if (response.twilioMessageId) {
+  if (message.id && response.twilioMessageId) {
     await prisma.message.update({
       where: {
-        id: message?.id,
+        id: message.id,
       },
       data: {
         twilioMessageId: response.twilioMessageId,
@@ -54,6 +54,8 @@ const findOrCreateMessage = async ({ contact, from, to, studioId }) => {
       },
     });
 
+    console.log({ message });
+
     if (message?.twilioMessageId) {
       console.log('Follow up message already sent');
       return null;
@@ -71,6 +73,8 @@ const findOrCreateMessage = async ({ contact, from, to, studioId }) => {
         },
       });
     }
+
+    console.log({ message });
 
     return message;
   } catch (error) {
