7edaf29eeb7053a1dfc30986d02d26681321a999
Kevin Andrews <andrewskevin92@gmail.com>
2024-05-01 06:11:56 -0700

    work on postgres

--- utils/prisma.js
+++ utils/prisma.js
@@ -1,7 +1,9 @@
-import { PrismaClient } from '@prisma/client/edge';
-import { withAccelerate } from '@prisma/extension-accelerate';
+// import { PrismaClient } from '@prisma/client/edge';
+import { PrismaClient } from '@prisma/client';
+// import { withAccelerate } from '@prisma/extension-accelerate';
 
-const prisma = new PrismaClient().$extends(withAccelerate());
+const prisma = new PrismaClient()
+// .$extends(withAccelerate());
 
 async function prismaQueryWrapper(query) {
     const result = await query;
