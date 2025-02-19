cf6179e4761ae1806c5cd6aaf77a27f454d3a5ad
= <andrewskevin92@gmail.com>
2025-02-19 10:56:46 -0800

    correct import

--- providers/PostHogProvider/index.js
+++ providers/PostHogProvider/index.js
@@ -1,6 +1,7 @@
 'use client';
 
-import posthog, { PostHogProvider as PHProvider } from 'posthog-js';
+import posthog from 'posthog-js';
+import { PostHogProvider as PHProvider } from 'posthog-js/react';
 import { useEffect } from 'react';
 
 export function PostHogProvider({ children }) {
