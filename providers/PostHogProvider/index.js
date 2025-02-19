34ca0ac4a057b445ba22a9426f6338bfa28a971f
= <andrewskevin92@gmail.com>
2025-02-19 10:53:14 -0800

    remove env.mjs import

--- providers/PostHogProvider/index.js
+++ providers/PostHogProvider/index.js
@@ -2,7 +2,6 @@
 
 import posthog, { PostHogProvider as PHProvider } from 'posthog-js';
 import { useEffect } from 'react';
-import { env } from '~/env.mjs';
 
 export function PostHogProvider({ children }) {
     useEffect(() => {
@@ -10,8 +9,8 @@ export function PostHogProvider({ children }) {
             !window.location.host.includes('127.0.0.1') &&
             !window.location.host.includes('localhost')
         ) {
-            posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY ?? '', {
-                api_host: env.NEXT_PUBLIC_POSTHOG_HOST,
+            posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY ?? '', {
+                api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
                 capture_pageview: false, // Disable automatic pageview capture, as we capture manually
                 capture_pageleave: true,
             });
